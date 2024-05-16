f990ff425fd3f08151471d0282bcec9296861c76
Deniied0 <>
2024-05-16 10:01:43 -0400

    ok

---
+++ go.mod
@@ -0,0 +1,7 @@
+module usernamegen
+
+go 1.21.9
+
+require github.com/fluhus/gostuff v0.4.0
+
+require golang.org/x/text v0.15.0 // indirect
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/fluhus/gostuff/nlp/wordnet"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	pattern := flag.String("pattern", "noun|verb|number", "Pattern for generating username")
+	numberlength := flag.Int("numberlength", 100, "length for numbers")
+	cleansymbols := flag.Bool("clean", true, "Remove symbols")
+	capitalize := flag.Bool("capitalize", true, "Capitalize each word in user")
+
+	flag.Parse()
+
+	wn, err := wordnet.Parse("wordnet-dict")
+	if err != nil {
+		fmt.Println("Error parsing WordNet:", err)
+		return
+	}
+
+	username := generateUsername(*pattern, *numberlength, *cleansymbols, *capitalize, wn)
+	fmt.Println("Random Username:", username)
+}
+
+func generateUsername(pattern string, numberlength int, cleansymbols bool, capitalize bool, wn *wordnet.WordNet) string {
+	parts := strings.Split(pattern, "|")
+	var username string
+
+	var usernameParts []string
+	for _, partType := range parts {
+		var part string
+
+		switch partType {
+		case "noun":
+			part = getRandomWord(wn, "n")
+		case "verb":
+			part = getRandomWord(wn, "v")
+		case "adjective":
+			part = getRandomWord(wn, "a")
+		case "adverb":
+			part = getRandomWord(wn, "r")
+		case "number":
+			part = fmt.Sprintf("%d", rand.Intn(numberlength))
+		default:
+			fmt.Println("Unknown pattern partType:", partType)
+		}
+
+		if capitalize {
+			part = cases.Title(language.English, cases.Compact).String(part)
+		}
+
+		usernameParts = append(usernameParts, part)
+	}
+
+	username = strings.Join(usernameParts, "")
+
+	if cleansymbols {
+		regex := regexp.MustCompile("[^a-zA-Z0-9]+")
+		username = regex.ReplaceAllString(username, "")
+	}
+
+	return username
+}
+
+func getRandomWord(wn *wordnet.WordNet, pos string) string {
+	words := make([]string, 0)
+	for word, idList := range wn.Lemma {
+		if strings.HasPrefix(word, pos) {
+			words = append(words, idList...)
+		}
+	}
+	if len(words) == 0 {
+		return ""
+	}
+	selectedID := words[rand.Intn(len(words))]
+	selectedSynset := wn.Synset[selectedID]
+	return selectedSynset.Word[0]
+}
